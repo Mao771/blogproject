b2e8f3ae02e6460e84efc6df943d4bccfe4f0493
Mao771 <max.mishchenko771@gmail.com>
2025-04-19 22:12:06 +0300

    Changed start.sh - added create superuser

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py wait_for_db  # optional: add custom command
+python manage.py createsuperuser --noinput
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
